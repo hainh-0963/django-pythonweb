7c03bff3b36283f4395f10a856b5e0acd89a82f1
hainh-0963 <nguyen.huu.hai@sun-asterisk.com>
2020-04-21 18:40:55 +0700

    Django Day 2

--- pythonweb/polls/views.py
+++ pythonweb/polls/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question, Choice
 
 
 # Create your views here.
 def index(request):
-    response = HttpResponse()
-    response.writelines("<h1>Hello</h1>")
-    response.write("You're at the polls index.")
-    return response
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
